32c48281cfc446574f395e9828e576c7209f73f4
dcekuhelu <dcekuhelu@gmail.com>
2022-09-19 15:52:52 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,26 +1,43 @@
-#include <stdio.h>
-#include "main.h"
+#include <stdio.h>                                                                           
 
-/**
- * puts2 - prints every other character
- * @str: string
- *
- * Return: nohing
- */
+#include "main.h"                                                                            
 
-void puts2(char *str)
-{
-	int i = 0;
+                                                                                             
 
-	while (*(str + i) != '\0')
-	{
-		if (i % 2 ==0)
-		
+/**                                                                                          
 
+ * puts2 - prints every other character                                                      
 
+ * @str: string                                                                              
 
-			putchar(*(str + i));
-		i++;
-	}
-	putchar(10);
+ *                                                                                           
+
+ * Return: nothing                                                                            
+
+ */                                                                                          
+
+                                                                                             
+
+void puts2(char *str)                                                                        
+
+{                                                                                            
+
+        int i = 0;                                                                           
+
+                                                                                             
+
+        while (*(str + i) != '\0')                                                           
+
+        {                                                                                    
+
+                if (i % 2 == 0)                                                               
+
+			putchar(*(str + i));                                                 
+
+                i++;                                                                         
+
+        }                                                                                    
+
+        putchar(10);                                                                         
+
 }
