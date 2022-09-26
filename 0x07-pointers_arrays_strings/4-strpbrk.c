f450e2656cadc84c1a03dbd616070826498bf32f
dcekuhelu <dcekuhelu@gmail.com>
2022-09-26 05:34:57 -0700

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,56 +3,55 @@
 #include <stdio.h>
 
 /**
- *
- *  * _strpbrk - prints the consecutive characters of s1 that are in s2.
- *
- *   * @s: source string
- *
- *    * @accept: searching string
- *
- *     * 
- *
- *      *  Return: new string.
- *
- *       */
+
+ * _strpbrk - prints the consecutive characters of s1 that are in s2.
+
+ * @s: source string
+
+ * @accept: searching string
+
+ * 
+
+ *  Return: new string.
+
+ */
 
 char *_strpbrk(char *s, char *accept);
 
 {
 
-		unsigned int i, j;
+	unsigned int i, j;
 
 
 
-			for (i = 0; *(s + i); i++)
+	for (i = 0; *(s + i); i++)
 
-					{
+	{
 
-								for (j = 0; *(accept + j); j++)
+		for (j = 0; *(accept + j); j++)
 
-											{
+		{
 
-															if (*(s + i) == *(accept + j))
+			if (*(s + i) == *(accept + j))
 
-																			{
+			{
 
-																								break;
+				break;
 
-																											}
+			}
 
-																	}
+		}
 
-										if (*(accept + j) != '\0')
+		if (*(accept + j) != '\0')
 
-													{
+		{
 
-																	return (s + i);
+			return (s + i);
 
-																			}
+		}
 
-											}
+	}
 
-				return (0);
+	return (0);
 
 }
-
